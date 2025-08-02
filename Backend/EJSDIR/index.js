fe238d70eef481c3ed92543282a261bef60fe64d
avinash9546 <sanjanabu47@gmail.com>
2025-08-03 01:14:41 +0530

    includes

--- Backend/EJSDIR/index.js
+++ Backend/EJSDIR/index.js
@@ -5,12 +5,28 @@ const app=express();
 
 const port=8080;
 
+app.use(express.static(path.join(__dirname, "public")));
+
+
 app.set("view engine","ejs");
-app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "/views"));
 
 app.get("/",(req,res)=>{
     res.render("home.ejs")
 })
+
+app.get("/ig/:username",(req,res)=>{
+    let username = req.params.username;
+    const insta=require("./data.json")
+    const data=insta[username];
+    if(data){
+    res.render("instagram.ejs",{data})
+    }
+    else{
+        res.render("error.ejs",{username})
+    }
+})
+
 app.get("/rolldice",(req,res)=>{
     let data=Math.floor(Math.random()*6) +1
     res.render("rolldice.ejs",{num:data})
